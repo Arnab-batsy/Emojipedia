cfbb629267e0919e20f60b2d68dd9b5d75cdfb4c
Arnab Kumar Das <78913766+Arnab-batsy@users.noreply.github.com>
2023-12-27 02:17:10 +0530

    Add the project files

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Entry from "./Components/Entry";
+import emojipedia from "./assets/emojipedia";
+
+function addTerms(akd) {
+  //The parameter can be any name
+  return (
+    <Entry
+      key={akd.id}
+      emoji={akd.emoji}
+      name={akd.name}
+      meaning={akd.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map(addTerms)}
+        {/* It's like each object of the emojipedia array will be passed as a parameter in the addTerms function*/}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
